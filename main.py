762c1a486dc763cda166edb0007f7cd524284aed
Antonio Claudio <claudioalvesdev@gmail.com>
2026-05-14 17:07:23 -0300

    refactor: update for langchain

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+
+from browser_use import Agent
+from browser_use.llm.google import ChatGoogle
+
+load_dotenv()
+
+
+async def main():
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+    llm = ChatGoogle(model="gemini-flash-latest", api_key=api_key)
+
+    agent = Agent(
+        task="Abra o Google Maps e pesquise por 'Pizzarias em São Paulo'",
+        llm=llm,
+    )
+
+    await agent.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
